64361900b5149348ae10096961cdea3fb6d208fc
osparking <jbpark03@gmail.com>
2023-02-24 23:16:53 +0900

    request handler uses todos bean

--- src/main/java/space/bumtiger/ssnAttr/demo/controller/TodoControllerWithScopedProxy.java
+++ src/main/java/space/bumtiger/ssnAttr/demo/controller/TodoControllerWithScopedProxy.java
@@ -1,9 +1,12 @@
 package space.bumtiger.ssnAttr.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.Data;
+import space.bumtiger.ssnAttr.demo.domain.TodoItem;
 import space.bumtiger.ssnAttr.demo.domain.TodoList;
 
 @Controller
@@ -11,5 +14,14 @@ import space.bumtiger.ssnAttr.demo.domain.TodoList;
 @Data
 public class TodoControllerWithScopedProxy {
 	private TodoList todos;
-
+	
+	@GetMapping("/form") 
+	public String showForm(Model model) {
+		if (!todos.isEmpty()) {
+			model.addAttribute("todo", todos.peekLast());
+		} else {
+			model.addAttribute("todo", new TodoItem());
+		}
+		return "scopedproxyform";
+	}
 }
