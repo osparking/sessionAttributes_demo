429ee7076deb8d27040d1f9875db887df5dd2510
osparking <jbpark03@gmail.com>
2023-02-24 14:23:19 +0900

    web-thymeleaf starter project

---
+++ src/main/java/space/bumtiger/ssnAttr/demo/SessionAttributesDemoApplication.java
@@ -0,0 +1,13 @@
+package space.bumtiger.ssnAttr.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionAttributesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionAttributesDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/bumtiger/ssnAttr/demo/SessionAttributesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bumtiger.ssnAttr.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionAttributesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
